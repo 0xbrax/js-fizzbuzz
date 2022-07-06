34b05f004b7217c804a29382308b07b09a32c7a8
Marco Braccini <marco.braccini@live.it>
2022-07-06 16:31:59 +0200

    Add different box background color To do: input for cycle

--- js/main.js
+++ js/main.js
@@ -1,31 +1,30 @@
 const items = document.getElementById('item-container');
 
 for (let i = 1; i <= 100; i++) {
-    console.log(i);
 
     if (i % 15 == 0) {
         //items.innerHTML += `<div class="box">${i} fizzbuzz</div>`;
         const obj = document.createElement('div');
         obj.append(i + ' fizzbuzz');
-        obj.className = 'box';
+        obj.className = 'box bg-red';
         items.append(obj);
     } else if (i % 5 == 0) {
         //items.innerHTML += `<div class="box">${i} buzz</div>`;
         const obj = document.createElement('div');
         obj.append(i + ' buzz');
-        obj.className = 'box';
+        obj.className = 'box bg-yellow';
         items.append(obj);
     } else if (i % 3 == 0) {
         //items.innerHTML += `<div class="box">${i} fizz</div>`;
         const obj = document.createElement('div');
         obj.append(i + ' fizz');
-        obj.className = 'box';
+        obj.className = 'box bg-green';
         items.append(obj);
     } else {
         //items.innerHTML += `<div class="box">${i}</div>`;
         const obj = document.createElement('div');
         obj.append(i);
-        obj.className = 'box';
+        obj.className = 'box bg-blue';
         items.append(obj);
     }
 }
