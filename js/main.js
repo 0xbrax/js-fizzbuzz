b75eb320a80105976352e7b7feed98f4d5bd11c1
Marco Braccini <marco.braccini@live.it>
2022-07-06 16:06:16 +0200

    Javascript program working w/ variant appen method

--- js/main.js
+++ js/main.js
@@ -4,12 +4,28 @@ for (let i = 1; i <= 100; i++) {
     console.log(i);
 
     if (i % 15 == 0) {
-        items.innerHTML += `<div class="box">${i} fizzbuzz</div>`;
+        //items.innerHTML += `<div class="box">${i} fizzbuzz</div>`;
+        const obj = document.createElement('div');
+        obj.append(i + ' fizzbuzz');
+        obj.className = 'box';
+        items.append(obj);
     } else if (i % 5 == 0) {
-        items.innerHTML += `<div class="box">${i} buzz</div>`;
+        //items.innerHTML += `<div class="box">${i} buzz</div>`;
+        const obj = document.createElement('div');
+        obj.append(i + ' buzz');
+        obj.className = 'box';
+        items.append(obj);
     } else if (i % 3 == 0) {
-        items.innerHTML += `<div class="box">${i} fizz</div>`;
+        //items.innerHTML += `<div class="box">${i} fizz</div>`;
+        const obj = document.createElement('div');
+        obj.append(i + ' fizz');
+        obj.className = 'box';
+        items.append(obj);
     } else {
-        items.innerHTML += `<div class="box">${i}</div>`;
+        //items.innerHTML += `<div class="box">${i}</div>`;
+        const obj = document.createElement('div');
+        obj.append(i);
+        obj.className = 'box';
+        items.append(obj);
     }
 }
