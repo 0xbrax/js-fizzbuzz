466ff39d834851bc1bce1ecf3991b052562232db
Marco Braccini <marco.braccini@live.it>
2022-07-06 16:45:57 +0200

    Add input text & button click event to generate elements To do: css customization

--- js/main.js
+++ js/main.js
@@ -1,30 +1,34 @@
 const items = document.getElementById('item-container');
+let data = document.getElementById('data-in');
+const btnGen = document.getElementById('btn-gen');
 
-for (let i = 1; i <= 100; i++) {
+btnGen.addEventListener('click', function() {
+    for (let i = 1; i <= data.value; i++) {
 
-    if (i % 15 == 0) {
-        //items.innerHTML += `<div class="box">${i} fizzbuzz</div>`;
-        const obj = document.createElement('div');
-        obj.append(i + ' fizzbuzz');
-        obj.className = 'box bg-red';
-        items.append(obj);
-    } else if (i % 5 == 0) {
-        //items.innerHTML += `<div class="box">${i} buzz</div>`;
-        const obj = document.createElement('div');
-        obj.append(i + ' buzz');
-        obj.className = 'box bg-yellow';
-        items.append(obj);
-    } else if (i % 3 == 0) {
-        //items.innerHTML += `<div class="box">${i} fizz</div>`;
-        const obj = document.createElement('div');
-        obj.append(i + ' fizz');
-        obj.className = 'box bg-green';
-        items.append(obj);
-    } else {
-        //items.innerHTML += `<div class="box">${i}</div>`;
-        const obj = document.createElement('div');
-        obj.append(i);
-        obj.className = 'box bg-blue';
-        items.append(obj);
+        if (i % 15 == 0) {
+            //items.innerHTML += `<div class="box">${i} fizzbuzz</div>`;
+            const obj = document.createElement('div');
+            obj.append(i + ' fizzbuzz');
+            obj.className = 'box bg-red';
+            items.append(obj);
+        } else if (i % 5 == 0) {
+            //items.innerHTML += `<div class="box">${i} buzz</div>`;
+            const obj = document.createElement('div');
+            obj.append(i + ' buzz');
+            obj.className = 'box bg-yellow';
+            items.append(obj);
+        } else if (i % 3 == 0) {
+            //items.innerHTML += `<div class="box">${i} fizz</div>`;
+            const obj = document.createElement('div');
+            obj.append(i + ' fizz');
+            obj.className = 'box bg-green';
+            items.append(obj);
+        } else {
+            //items.innerHTML += `<div class="box">${i}</div>`;
+            const obj = document.createElement('div');
+            obj.append(i);
+            obj.className = 'box bg-blue';
+            items.append(obj);
+        }
     }
-}
+});
